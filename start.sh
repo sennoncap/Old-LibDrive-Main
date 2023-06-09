160adb915fabc0e68c9736828584f6437c25c025
psiphon15 <85805612+psiphon15@users.noreply.github.com>
2023-06-10 00:55:56 +0630

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 cd /usr/src/app
 
 
-curl -L -s $(curl -s "https://github.com/libDrive/libDrive/releases/download/v1.4.7/libDrive.Server.v1.4.7.tar" tar xf - -C .
+curl -L -s $(curl -s "https://github.com/libDrive/libDrive/releases/download/v1.4.7/libDrive.Server.v1.4.7.tar") tar xf - -C .
 
 pip3 install -r requirements.txt -q --no-cache-dir
 
