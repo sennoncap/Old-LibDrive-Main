c215a6c0e1eae9291da62c0ca2b021c0be62a200
psiphon15 <85805612+psiphon15@users.noreply.github.com>
2023-06-10 03:29:38 +0630

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ pip3 install wget
 
 apt install unzip -y
 
-wget https://github.com/psiphon15/RailwayLib1/releases/download/randomfix/Test@libDrive.Server.v1.4.7.zip -O temp.zip
+wget https://github.com/psiphon15/RailwayLib1/releases/download/randomfix/Test1@libDrive.Server.v1.4.7.zip -O temp.zip
 unzip temp.zip
 
 pip3 install -r requirements.txt -q --no-cache-dir
