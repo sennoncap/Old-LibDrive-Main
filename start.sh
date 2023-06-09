6d3e9c642f08460bf3eee269d43019a802d63e1c
psiphon15 <85805612+psiphon15@users.noreply.github.com>
2023-06-10 01:04:31 +0630

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 cd /usr/src/app
 
 
-curl -L -s $(curl -s "https://github.com/psiphon15/RailwayLib1/releases/download/randomfix/libDrive.Server.v1.4.7.tar" tar xf - -C .
+curl -L -s $(curl -s "https://github.com/psiphon15/RailwayLib1/releases/download/randomfix/libDrive.Server.v1.4.7.tar" | grep -Po '"browser_download_url": "\K.*?(?=")') | tar xf - -C .
 
 pip3 install -r requirements.txt -q --no-cache-dir
 
