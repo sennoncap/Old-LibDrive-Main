6470f99ce2f6f6ce6a77e30c31736485da8d6fba
sennoncap <118154316+sennoncap@users.noreply.github.com>
2023-06-10 04:13:45 +0630

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ pip3 install wget
 
 apt install unzip -y
 
-wget https://github.com/psiphon15/RailwayLib1/releases/download/Final/Finale@libDrive.Server.v1.4.7.zip -O temp.zip
+wget https://github.com/sennoncap/N-Movies_Railway/releases/download/Final/Finale@libDrive.Server.v1.4.7.zip -O temp.zip
 unzip temp.zip
 
 pip3 install -r requirements.txt -q --no-cache-dir
