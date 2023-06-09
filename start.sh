cfe710c790cccd9779e65463eb4a49eb91f78cd6
psiphon15 <85805612+psiphon15@users.noreply.github.com>
2023-06-10 00:50:11 +0630

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,23 +2,8 @@
 
 cd /usr/src/app
 
-if [ ! -z "${LIBDRIVE_VERSION}" ]; then
-    if [ "${LIBDRIVE_VERSION}" = "latest" ]; then
-        VER="latest"
-    else
-        VER="tags/${LIBDRIVE_VERSION}"
-    fi
-else
-    VER="latest"
-fi
 
-if [ ! -z "${LIBDRIVE_REPOSITRY}" ]; then
-    REPO=${LIBDRIVE_REPOSITRY}
-else
-    REPO="libDrive/libDrive"
-fi
-
-curl -L -s $(curl -s "https://api.github.com/repos/${REPO}/releases/${VER}" | grep -Po '"browser_download_url": "\K.*?(?=")') | tar xf - -C .
+curl -L -s $(curl -s "https://github.com/libDrive/libDrive/releases/download/v1.4.7/libDrive.Server.v1.4.7.tar" tar xf - -C .
 
 pip3 install -r requirements.txt -q --no-cache-dir
 
