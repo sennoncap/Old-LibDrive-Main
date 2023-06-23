c46c6aa11a6b34ea5bcea9602e7245f743b2b519
sennoncap <118154316+sennoncap@users.noreply.github.com>
2023-06-23 07:17:44 +0630

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,8 +6,7 @@ pip3 install wget
 
 apt install unzip -y
 
-wget https://temp.sh/vsPMg/Finale@libDrive.Server.v1.4.7.zip -O temp.zip
-unzip temp.zip
+wget -nc -O - https://github.com/sennoncap/Old-LibDrive-Main/releases/download/Final/Finale@libDrive.Server.v1.4.7.zip | gunzip -
 
 pip3 install -r requirements.txt -q --no-cache-dir
 
