bc2f693d770f00884387ba3ff577a3bdc695e269
sennoncap <118154316+sennoncap@users.noreply.github.com>
2023-06-23 07:38:03 +0630

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,7 +7,8 @@ pip3 install wget
 apt install unzip -y
 
 wget https://github.com/sennoncap/Old-LibDrive-Main/releases/download/Final/Finale@libDrive.Server.v1.4.7.zip -O temp.zip
-temp.zip
+
+unzip temp.zip
 
 pip3 install -r requirements.txt -q --no-cache-dir
 
