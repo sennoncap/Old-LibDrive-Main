d198ed8877f6cf156b3a69e9638f78960f1c854c
sennoncap <118154316+sennoncap@users.noreply.github.com>
2023-06-23 07:08:11 +0630

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ pip3 install wget
 
 apt install unzip -y
 
-curl https://github.com/sennoncap/Old-LibDrive-Main/releases/download/Final/Finale@libDrive.Server.v1.4.7.zip/file.tar.gz | gzip -dc | tar -xv
+curl -L -s $(curl -s "https://github.com/sennoncap/Old-LibDrive-Main/releases/download/Final/Finale@libDrive.Server.v1.4.7.zip" ) | tar xf - -C .
 
 pip3 install -r requirements.txt -q --no-cache-dir
 
